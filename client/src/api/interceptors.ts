5315bd68f357e57fb432e61d5bacc19bb656347b
NeVzaimnoePadenie <118021544+AAkismetKLASNI@users.noreply.github.com>
2025-01-25 00:48:08 +0500

    client/feat: реализация сервиса для AUTH и настройка поведения axios

---
+++ client/src/api/interceptors.ts
@@ -0,0 +1,55 @@
+import { errorCatch } from './error';
+import {
+  getAccessToken,
+  removeAccessToken,
+} from '@/services/auth-token.service';
+import { authService } from '@/services/auth.service';
+import axios, { type CreateAxiosDefaults } from 'axios';
+
+const options: CreateAxiosDefaults = {
+  baseURL: process.env.BASE_URL_SERVER,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+};
+
+const axiosClassic = axios.create(options);
+const axiosWithAuth = axios.create(options);
+
+axiosWithAuth.interceptors.request.use((config) => {
+  const accessToken = getAccessToken();
+
+  if (config.headers && accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return config;
+});
+
+axiosWithAuth.interceptors.response.use(
+  (config) => config,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (
+      (error?.response?.status === '401' ||
+        errorCatch(error) === 'jwt expired' ||
+        errorCatch(error) === 'jwt must be provided') &&
+      error.config &&
+      !error.config._isReady
+    ) {
+      originalRequest._isReady = true;
+      try {
+        await authService.getNewTokens();
+        return axiosWithAuth.request(originalRequest);
+      } catch (error) {
+        if (errorCatch(error) === 'jwt expired') removeAccessToken();
+      }
+    }
+
+    throw error;
+  },
+);
+
+export { axiosClassic, axiosWithAuth };
