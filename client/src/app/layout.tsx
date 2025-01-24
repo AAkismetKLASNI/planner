f3ef77f8a0610b62e0498bbafa7e804795e025b0
NeVzaimnoePadenie <118021544+AAkismetKLASNI@users.noreply.github.com>
2025-01-24 21:22:39 +0500

    client/init: инициализация клиента и общая настройка

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,34 @@
+import { Providers } from './providers';
+import type { Metadata } from 'next';
+import { Noto_Sans } from 'next/font/google';
+import { Toaster } from 'sonner';
+import { SITE_NAME } from '@/constants/seo.constants';
+import './globals.scss';
+
+const notoSans = Noto_Sans({
+  subsets: ['latin', 'cyrillic'],
+  display: 'swap',
+  variable: '--font-noto-sans',
+  style: 'normal',
+  weight: ['300', '400', '500', '600', '700'],
+});
+
+export const metadata: Metadata = {
+  title: { default: SITE_NAME, template: `%s | ${SITE_NAME}` },
+  description: 'Best one for planning from reflection-planner',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${notoSans.className} antialiased`}>
+        <Providers>{children}</Providers>
+        <Toaster theme="dark" position="bottom-right" />
+      </body>
+    </html>
+  );
+}
